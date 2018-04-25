4797a91f9ca1de4cb41620783aed58209f743a6e
laraismael1 <laraismael123@gmail.com>
2018-04-25 11:10:06 -0700

    update packge.json to support redux-form

--- src/js/components/container/setupTrip.js
+++ src/js/components/container/setupTrip.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import { reduxForm, Field } from 'redux-form';
+// reduxForm brings the funcion that will super charge the form
+// Field is used to bring a fild that can be specified with its attributes
 class SetupTrip extends Component {
 	render() {
 		return (
@@ -7,21 +9,21 @@ class SetupTrip extends Component {
 				<div className="form-field">
 					<label>Where are you going?</label>
 					<div>
-						<input type="text" name="where" />
+						<Field type="text" component="input" name="where" />
 					</div>
 				</div>
 
 				<div className="form-field">
 					<label>When do you plan to go?</label>
 					<div>
-						<input type="text" name="when" />
+						<Field type="text" component="input" name="when" />
 					</div>
 				</div>
 
 				<div className="form-field">
 					<label>Who is coming?</label>
 					<div>
-						<input type="text" name="who" />
+						<Field type="text" component="input" name="who" />
 					</div>
 				</div>
 
@@ -31,4 +33,9 @@ class SetupTrip extends Component {
 	}
 }
 
-export default SetupTrip;
+// redux form works as a function that super charges the form by calling the component as its argument. The form value inside the reduxForm function can be anmed anything as long as it is uniqu among other forms in this project.
+
+// <Field type="text" component="input" name="where" />
+export default reduxForm({
+	form: 'SetupTrip'
+})(SetupTrip);
